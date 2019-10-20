3d7e26129bd16186f7acfe713d0a0a60cd854af7
miordach <mihai.iordache@misys.com>
2019-10-20 18:08:20 +0300

    wip

--- src/app/restaurant-menu/menu-products/menu-products.page.ts
+++ src/app/restaurant-menu/menu-products/menu-products.page.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-menu-products',
-  templateUrl: './menu-products.page.html',
-  styleUrls: ['./menu-products.page.scss'],
+  selector: "app-menu-products",
+  templateUrl: "./menu-products.page.html",
+  styleUrls: ["./menu-products.page.scss"]
 })
 export class MenuProductsPage implements OnInit {
   menu: any = null;
+  searchTerm = '';
+  products: any;
+  filteredProducts: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.snapshot.data.menu.menuProducts.map(prod => {
       prod.showDetails = false;
     });
     this.menu = this.route.snapshot.data.menu;
+    this.products = this.menu.menuProducts;
+    this.filterItems(this.searchTerm);
   }
 
   addProduct(prod) {
     console.log(prod);
   }
 
+  filterItems(searchTerm) {
+    this.filteredProducts = this.products.filter(item => {
+      return item.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+  }
 }
