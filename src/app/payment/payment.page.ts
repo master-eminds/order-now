47e916f145a7f5987c937edf1fb11d4ad467921b
miordach <mihai.iordache@misys.com>
2019-11-11 22:32:52 +0200

    test

--- src/app/payment/payment.page.ts
+++ src/app/payment/payment.page.ts
@@ -1,10 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { formatDate } from '@angular/common';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl
+} from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
 const format = 'yyyy-MM-dd';
 const locale = 'en-US';
 
+function cardValidator(c: AbstractControl): { [key: string]: boolean } | null {
+  const cardno = /^(?:4[0-9]{12}(?:[0-9]{3})?)$/;
+  if (c.value && !c.value.match(cardno)) {
+    return { card: true};
+  } else {
+    return null;
+  }
+
+  //create an async validator using backend check
+}
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.page.html',
@@ -13,6 +28,7 @@ const locale = 'en-US';
 export class PaymentPage implements OnInit {
   paymentForm: FormGroup;
   minDate = '';
+  cardMessage: string;
   monthNames = [
     'Jan',
     'Feb',
@@ -27,6 +43,10 @@ export class PaymentPage implements OnInit {
     'Nov',
     'Dec'
   ];
+  private validationMessages = {
+    required: 'Please enter your card number.',
+    card: 'Please enter a valid card.'
+  };
 
   constructor(private fb: FormBuilder) {}
   /**
@@ -40,17 +60,25 @@ export class PaymentPage implements OnInit {
     console.log(this.minDate);
     this.paymentForm = this.fb.group({
       card_holder: ['', Validators.required],
-      card_number: ['', [Validators.required, Validators.minLength(13), Validators.maxLength(13)]],
+      card_number: ['', [Validators.required, cardValidator]],
       card_expiration_date: ['', [Validators.required]],
-      card_cvc: ['',[Validators.required, Validators.minLength(3), Validators.maxLength(3)]]
+      card_cvc: [
+        '',
+        [Validators.required, Validators.minLength(3), Validators.maxLength(3)]
+      ]
     });
 
     const card_number = this.paymentForm.get('card_number');
-    card_number.valueChanges.pipe(
-      debounceTime(1000)
-    ).subscribe((res) => {
-      console.log(res);
+    card_number.valueChanges.pipe(debounceTime(1000)).subscribe(res => {
+      this.setMessage(card_number);
     });
+  }
 
+  setMessage(c) {
+    this.cardMessage = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      this.cardMessage = Object.keys(c.errors).map(
+        key => this.validationMessages[key]).join(' ');
+    }
   }
 }
